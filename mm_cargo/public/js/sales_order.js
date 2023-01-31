37e0a34fa37c72f0e9fa44f2fc178f7ac011e5eb
Rohit Kadlak <rkadlak@dexciss.com>
2023-01-31 18:43:55 +0530

    fix:status

--- mm_cargo/public/js/sales_order.js
+++ mm_cargo/public/js/sales_order.js
@@ -3,7 +3,7 @@ frappe.ui.form.on("Sales Order", {
 
         frm.add_custom_button(__('Waybill'), function() {
 
-                frappe.new_doc('Waybill', {"delivery_customer": frm.doc.customer,"sales_order":frm.doc.name,"delivery_address_name":frm.doc.customer_address,"delivery_contact_name":frm.doc.contact_person}).then(function(r){
+                frappe.new_doc('Waybill', {"delivery_customer": frm.doc.customer,"sales_order":frm.doc.name,"pickup_address_name":frm.doc.company_address,"delivery_address_name":frm.doc.customer_address,"delivery_contact_name":frm.doc.contact_person}).then(function(r){
                     // let k = frappe.get_doc("")
                     $.each(frm.doc.milestone_list,function(i,v){
                     cur_frm.add_child("milestone_list",
